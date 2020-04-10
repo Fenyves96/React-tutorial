941cfebca581384ecc0817f2e25554456ed69761
Fenyves96 <fenyves96@hotmail.com>
2020-04-10 16:39:40 +0200

    tutorial folder for tutorial 1-40

--- src/App.js
+++ src/App.js
@@ -1,40 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
-import Greet from './components/Greet'
-import Welcome from './components/Welcome'
-import Message from './components/Message'
-import Counter from './components/Counter'
-import FunctionClick from './components/FunctionClick'
-import ClassClick from './components/ClassClick'
-import EventBind from './components/EventBind'
-import ParentComponent from './components/ParentComponent';
-import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList';
-import Stylesheet from './components/Stylesheet';
-import Inline from './components/Inline';
-import './appStyles.css'
-import styles from './appStyles.module.css'
-import Form from './components/Form';
-import LifecycleParent from './components/LifecycleParent';
-import FragmentDemo from './components/FragmentDemo';
-import Table from './components/Table';
-import PureComp from './components/PureComp';
-import RegComp from './components/RegComp';
-import ParentRegComp from './components/ParentRegComp';
-import RefsDemo from './components/RefsDemo';
-import FocusInput from './components/FocusInput';
-import FRParentInput from './components/FRParentInput';
-import PortalDemo from './PortalDemo';
-import Hero from './components/Hero'
-import ErrorBoundary from './components/ErrorBoundary';
-import ClickCounter from './components/ClickCounter';
-import HoverCounter from './components/HoverCounter';
-import User from './components/User';
-import HoverCounterTwo from './components/HoverCounterTwo';
-import ClickCounterTwo from './components/ClickCounterTwo';
-import ComponentC from './components/ComponentC';
-import { UserProvider } from './components/UserContext';
+
+import ComponentC from './components/tutorial/ComponentC';
+import { UserProvider } from './components/tutorial/UserContext';
 
 
 function App() {
